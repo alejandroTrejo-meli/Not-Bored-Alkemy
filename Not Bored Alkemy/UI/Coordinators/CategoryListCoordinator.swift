3766417eabe52b9f7aaf76410018b9ada2e8c18a
Julian Allisio <julian.allisio@mercadolibre.com>
2022-06-07 10:05:01 -0300

    Add category list files
    Add CategoryListViewController (and its xib file) and CategoryListCoordinator

---
+++ Not Bored Alkemy/UI/Coordinators/CategoryListCoordinator.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryListCoordinator.swift
+//  Not Bored Alkemy
+//
+//  Created by Mariano Julian Allisio on 07/06/2022.
+//
+
+import UIKit
+
+
+class CategoryListCoordinator: Coordinator {
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+}

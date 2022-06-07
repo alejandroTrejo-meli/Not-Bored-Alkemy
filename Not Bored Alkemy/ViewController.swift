49aeaeef7ed12d8133b14b4051261185d2a4be4a
Alejandro Trejo <alejadro.trejo@mercadolibre.con.mx>
2022-06-06 19:12:30 -0500

    Initial Commit

---
+++ Not Bored Alkemy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Not Bored Alkemy
+//
+//  Created by Alejandro Alejandro Trejo Rodriguez on 06/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
